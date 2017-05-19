2f1332099ae0ffa5577cd0d5c128a59c61800049
kitamarekweb <kitamarekweb@gmail.com>
2017-05-19 22:26:40 +0200

    Zone file

--- Models/Zone.js
+++ Models/Zone.js
@@ -1,1 +1,9 @@
 var mongoose = require('mongoose')
+
+var ZoneSchema = new mongoose.Schema({
+  name: {},
+  location: {},
+  timestamp: {}
+})
+
+module.exports = mongoose.model('ZoneSchema', ZoneSchema)
