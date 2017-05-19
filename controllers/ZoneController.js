1fd9c9ddacfcf6847a241a9e0ac56c52e81aee11
kitamarekweb <kitamarekweb@gmail.com>
2017-05-19 22:46:02 +0200

    find method

---
+++ controllers/ZoneController.js
@@ -0,0 +1,26 @@
+var Zone = require('../Models/Zone')
+
+module.exports = {
+
+  find: function (params, callback) {
+    Zone.find(params, function (err, zones) {
+      if (err){
+        callback(err, null)
+        return
+      }
+      callback(null, zones)
+    })
+  },
+  
+  findById: function () {
+    
+  },
+  
+  create: function () {
+    
+  },
+  
+  update: function () {
+    
+  }
+}
