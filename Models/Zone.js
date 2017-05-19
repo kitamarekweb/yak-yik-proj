522dfbc32000f8d14abd79071b9c41d6f4c10451
kitamarekweb <kitamarekweb@gmail.com>
2017-05-19 22:31:22 +0200

    Zone file edited

--- Models/Zone.js
+++ Models/Zone.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose')
 
 var ZoneSchema = new mongoose.Schema({
-  name: {},
-  location: {},
-  timestamp: {}
+  name: {type: String, default: ''},
+  zipCodes: {type: Array, default: []},
+  timestamp: {type: Date, default: Date.now}
 })
 
 module.exports = mongoose.model('ZoneSchema', ZoneSchema)
