188e68e5f73e0cf68263246b5763a2b2ff9a9556
kitamarekweb <kitamarekweb@gmail.com>
2017-05-20 15:40:08 +0200

    edited webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,21 @@ var path = require('path')
 
 module.exports = {
   entry: {
-
+    app: './src/app.js'
   },
   output: {
-
+    filename: 'public/build/bundel.js',
+    sourceMapFilename: 'public/build/bundle.map'
   },
   devtool: '#source-map',
   module: {
-
+    loaders: [
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    ]
   }
 }
