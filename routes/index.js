4c7187daa7218ad0c22fec9ec5cf97d8f374a61c
kitamarekweb <kitamarekweb@gmail.com>
2017-05-20 13:26:25 +0200

    created route to page /createzone

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET createZone page. */
+router.get('/createzone', function(req, res, next) {
+  res.render('createzone', null);
+});
+
 module.exports = router;
