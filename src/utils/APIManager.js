256169a2fa0267eac587ab39aba90e45456365e0
kitamarekweb <kitamarekweb@gmail.com>
2017-05-22 15:30:34 +0200

    created REQUEST and API failure for GET method

---
+++ src/utils/APIManager.js
@@ -0,0 +1,42 @@
+import superagent from 'superagent'
+
+export default {
+
+  get: (url, params, callback) => {
+    superagent
+      .get(url)
+      .query(params)
+      .set('Accept', 'application/json')
+      .end((err, response) => {
+
+        if(err){
+          callback(err, null) //this is a REQUEST failure, NOT api failure
+          return
+        }
+
+        //this is API failure, when connfirmation will be 'fail'
+        const confirmation = response.body.confirmation
+        if (confirmation != 'success'){
+          callback({message: response.body.message}, null)
+          return
+        }
+
+        callback(null, response.body) //response.body its the thing that comes form SUPERAGENT
+
+      })
+
+  },
+
+  post: () => {
+
+  },
+
+  put: () => {
+
+  },
+
+  delete: () => {
+
+  }
+
+}
