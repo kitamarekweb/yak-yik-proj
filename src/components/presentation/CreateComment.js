def3a67ba82d831030cc5b7a600afc277d5c783f
kitamarekweb <kitamarekweb@gmail.com>
2017-05-24 12:48:28 +0200

    created submitComment function and onClick event

--- src/components/presentation/CreateComment.js
+++ src/components/presentation/CreateComment.js
@@ -4,9 +4,7 @@ class CreateComment extends Component {
   constructor(){
     super()
     this.state = {
-      comment: {
-
-      }
+      comment: {}
     }
   }
 
@@ -21,13 +19,17 @@ class CreateComment extends Component {
     })
   }
 
+  submitComment(event){
+    console.log('submitComment: ' +JSON.stringify(this.state.comment))
+  }
+
   render(){
     return(
       <div>
         <h3>Create Comment</h3>
         <input id="username" onChange={this.updateComment.bind(this)} className="form-control" type="text" placeholder="Username"/><br/>
         <input id="body" onChange={this.updateComment.bind(this)} className="form-control" type="text" placeholder="Comment"/><br/>
-        <button className="btn btn-info">Submit Comment</button>
+        <button onClick={this.submitComment.bind(this)} className="btn btn-info">Submit Comment</button>
       </div>
     )
   }
