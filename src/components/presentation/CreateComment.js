b58bed1e78ad5b95d17795e850ed47efe5137e60
kitamarekweb <kitamarekweb@gmail.com>
2017-05-24 12:23:26 +0200

    removed functions 'onClick' because they didn't have data to work on

--- src/components/presentation/CreateComment.js
+++ src/components/presentation/CreateComment.js
@@ -5,9 +5,9 @@ class CreateComment extends Component {
     return(
       <div>
         <h3>Create Comment</h3>
-        <input onChange={this.updateUsername.bind(this)} className="form-control" type="text" placeholder="Username"/><br/>
-        <input onChange={this.updateBody.bind(this)} className="form-control" type="text" placeholder="Comment"/><br/>
-        <button onClick={this.submitComment.bind(this)} className="btn btn-info">Submit Comment</button>
+        <input className="form-control" type="text" placeholder="Username"/><br/>
+        <input className="form-control" type="text" placeholder="Comment"/><br/>
+        <button className="btn btn-info">Submit Comment</button>
       </div>
     )
   }
