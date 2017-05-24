ead283eead8d11be705a9ea2f783aff9fa83379f
kitamarekweb <kitamarekweb@gmail.com>
2017-05-24 12:32:58 +0200

    creating events onChange for username and body

--- src/components/presentation/CreateComment.js
+++ src/components/presentation/CreateComment.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 
 class CreateComment extends Component {
+  constructor(){
+    super()
+    this.state = {
+      comment: {
+
+      }
+    }
+  }
+
+
+  updateComment(event){
+    console.log('updateComment: '+ event.target.id) + '==' + event.target.value
+  }
+
   render(){
     return(
       <div>
         <h3>Create Comment</h3>
-        <input className="form-control" type="text" placeholder="Username"/><br/>
-        <input className="form-control" type="text" placeholder="Comment"/><br/>
+        <input id="username" onChange={this.updateComment.bind(this)} className="form-control" type="text" placeholder="Username"/><br/>
+        <input id="body" onChange={this.updateComment.bind(this)} className="form-control" type="text" placeholder="Comment"/><br/>
         <button className="btn btn-info">Submit Comment</button>
       </div>
     )
