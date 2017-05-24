b022e5cd15b30f899e19f6dc415c8c22368b7ea6
kitamarekweb <kitamarekweb@gmail.com>
2017-05-24 13:40:45 +0200

    created functions updateZone and submitZone and events onChange and onClick

--- src/components/presentation/CreateZone.js
+++ src/components/presentation/CreateZone.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
 
 class CreateZone extends Component {
+  constructor(){
+    super()
+    this.state = {
+      zone: {}
+    }
+  }
+
+  updateZone(event){
+    let updated = Object.assign({}, this.state.zone)
+    updated[event.target.id] = event.target.value
+    this.setState({
+      zone: updated
+    })
+  }
+
+  submitZone(event){
+    console.log('submitZone: ' +JSON.stringify(this.state.zone))
+  }
+
   render() {
     return (
       <div>
-        <input id="name" className="form-control" type="text" placeholder="Name"/><br/>
-        <input id="zipCode" className="form-control" type="text" placeholder="Zip Code"/><br/>
-        <button className="btn btn-danger">Add Zone</button>
+        <h3>Create Zone</h3>
+        <input id="name" onChange={this.updateZone.bind(this)} className="form-control" type="text" placeholder="Name"/><br/>
+        <input id="zipCode" onChange={this.updateZone.bind(this)} className="form-control" type="text" placeholder="Zip Code"/><br/>
+        <button onClick={this.submitZone.bind(this)} className="btn btn-danger">Add Zone</button>
       </div>
     )
   }
